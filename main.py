50b3c8b2056f78b8a09442e8f4698320085112aa
rodriguezejl1 <rodriguezejl1@gmail.com>
2023-06-08 13:38:25 -0400

    nuevo commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+num1 = input('ingrese numero uno: ')
+num2 = input('ingrese numero dos: ')
+
+if (num1 > num2 ):
+    result =  int(num1) + int(num2)
+else:
+    result = int(num1) - int(num2)
+    
+print(result)
+
+"""
+
+"""
+i = 0
+
+while i < 5:
+	i = i + 	1
+	print('La variable vale',i)
+
+"""	
+
+i = 1
+while i < 6:
+  #print(i)
+  i += 1
+  if i == 3:
+  	print("se detiene en 3")
+    break
+  
